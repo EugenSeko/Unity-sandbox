788664176d6c1d0e4661827c990ae43e88f08dd3
DESKTOP-ERGRU5L\Eugen <Satyagrah108@gmail.com>
2017-09-19 21:29:07 +0300

    Add chat

---
+++ Assets/Scripts/PanicLab/Multiplayer/GameHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameHelper : MonoBehaviour {
+
+    public InputField input;
+    public Text textBlock;
+    private PlayerHelper _currentPlayer;
+
+    public PlayerHelper currentPlayer { get { return _currentPlayer; } set { _currentPlayer = value; } }
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    public void Send()
+    {
+        _currentPlayer.Send(input.text);
+    }
+    public void SetDeactiveCanvas()
+    {
+        GameObject.FindGameObjectWithTag("canvas").SetActive(false);
+        GameObject.FindGameObjectWithTag("MainCamera").transform.position = new Vector3(0, 0, -100);
+        if (_currentPlayer.isServer)
+        {
+            Debug.Log("SERVER");
+        }
+    }
+}
---
+++ Assets/Scripts/PanicLab/Multiplayer/PlayerHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerHelper : NetworkBehaviour {
+
+    private GameHelper _gameHelper;
+
+	// Use this for initialization
+	void Start () {
+        _gameHelper = GameObject.FindObjectOfType<GameHelper>();
+
+        if (isLocalPlayer)
+        {
+            _gameHelper.currentPlayer = this;//передача ссылки в свойство currentPlayer на этот объект.
+        }
+	}
+
+    public void Send(string message)
+    {
+        CmdSend(Network.player.guid, message);
+    }
+
+    [Command]
+    public void CmdSend(string id, string message)
+    {
+        int rand = Random.Range(0, 100);
+
+        Debug.Log("Message: " + message+"/"+id+"/"+rand);
+        RpcSend(id, message, rand);
+    }
+    [ClientRpc]
+    public void RpcSend(string id, string message, int random)
+    {
+        _gameHelper.textBlock.text += System.Environment.NewLine + "id:"+id+"<<"+message+">>"+"/"+random;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
